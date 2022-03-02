6f8b691ae7e9920bf84f795761c2b65f433e904f
arrxy <87611211+arrxy@users.noreply.github.com>
2022-03-03 00:05:35 +0530

    Add files via upload

---
+++ GRAPH/dijsktra.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int 					long long
+#define endl 					'\n'
+#define vb						vector<bool>
+#define vbb						vector<vb>
+#define vi 						vector<int>
+#define vvi 					vector<vector<int>>
+#define inf						1e18
+#define setbits(x)				__builtin_popcountll(x)
+#define loop(i,a,b)				for(int i = a; i<=b; i+=1)
+#define revloop(i,a,b)			for(int i = a; i>=b; i-=1)
+#define pii						pair<int,int>
+#define w(x)					int x; cin >> x; while(x--)
+#define pb 						push_back
+#define ust						unordered_set
+#define ump						unordered_map
+#define ff 						first
+#define ss  				    second
+#define bug(...)       			__f (#__VA_ARGS__, __VA_ARGS__)
+#define printV(v)				for(auto &it: v) cout<<it<<" "; cout<<endl;
+#define all(v)      			v.begin(),v.end()
+#define inp(i)  				int i; cin >> i;
+#define inp2(i,j)				int i,j; cin >> i >> j;
+#define inp3(i,j,k)				int i,j,k; cin >> i >> j >> k;
+#define inp4(i,j,k,l)			int i,j,k,l; cin >> i >> j >> k >> l;
+#define inparr(arr, n)			inp(n); vi arr(n); for(auto &it: arr) cin >> it;
+#define MOD						1000000007ll
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+int dijsktra(vector<pair<int, int>> adj[], int src, int dest, int n) {
+	vi dist(n + 1, inf);
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push({0, src});
+	dist[src] = 0;
+	while (!pq.empty())
+	{
+		pii top = pq.top();
+		pq.pop();
+		int node = top.ss;
+		int d = top.ff;
+		for (auto &it : adj[node]) {
+			if (dist[it.ff] > d + it.ss) {
+				pq.push({d + it.ss, it.ff});
+				dist[it.ff] = d + it.ss;
+			}
+		}
+	}
+	return dist[dest];
+	return 0;
+}
+void solve()
+{
+	inp2(n, m);
+	vector<pii> adj[n + 1];
+	loop(i, 1, m)
+	{
+		inp3(u, v, wt);
+		adj[v].pb(make_pair(u, wt));
+		adj[u].pb(make_pair(v, wt));
+	}
+	inp2(src, dest);
+	cout << dijsktra(adj, src, dest, n) << endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	clock_t z = clock();
+	//w(x)
+	{
+		solve();
+	}
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+
+}
