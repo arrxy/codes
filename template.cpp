ba8d068f7209d79033aeea49aebe32a831a0bfdd
arrxy <87611211+arrxy@users.noreply.github.com>
2022-06-13 20:17:11 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,33 +1,37 @@
+/*
+author: arrxy
+*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-#define int 									long long
-#define endl 									'\n'
-#define vb									vector<bool>
-#define vbb									vector<vb>
-#define vi 									vector<int>
-#define vvi 									vector<vector<int>>
-#define inf									1e18
-#define setbits(x)								__builtin_popcountll(x)
-#define loop(i,a,b)								for(int i = a; i<=b; i+=1)
-#define revloop(i,a,b)								for(int i = a; i>=b; i-=1)
-#define pii									pair<int,int>
-#define w(x)									int x; cin >> x; while(x--)
-#define pb 									push_back
-#define ust									unordered_set
-#define ump									unordered_map
-#define ff 									first
-#define ss  				   				        second
-#define bug(...)       								__f (#__VA_ARGS__, __VA_ARGS__)
-#define printV(v)								for(auto &it: v) cout<<it<<" "; cout<<endl;
-#define all(v)      								v.begin(),v.end()
-#define inp(i)  								int i; cin >> i;
-#define inp2(i,j)								int i,j; cin >> i >> j;
-#define inp3(i,j,k)								int i,j,k; cin >> i >> j >> k;
-#define inp4(i,j,k,l)								int i,j,k,l; cin >> i >> j >> k >> l;
-#define inparr(arr, n)								inp(n); vi arr(n); for(auto &it: arr) cin >> it;
-#define MOD									1000000007ll
-
+#define int                         long long
+#define endl                        '\n'
+#define vb                          vector<bool>
+#define vbb                         vector<vb>
+#define vi                          vector<int>
+#define vvi                         vector<vector<int>>
+#define inf                         1e18
+#define setbits(x)                  __builtin_popcountll(x)
+#define loop(i,a,b)                 for(int i = a; i<=b; i+=1)
+#define revloop(i,a,b)              for(int i = a; i>=b; i-=1)
+#define pii                         pair<int,int>
+#define w(x)                        int x; cin >> x; while(x--)
+#define pb                          push_back
+#define ust                         unordered_set
+#define ump                         unordered_map
+#define ff                          first
+#define ss                          second
+#define bug(...)                    __f (#__VA_ARGS__, __VA_ARGS__)
+#define printV(v)                   for(auto &it: v) cout<<it<<" "; cout<<endl;
+#define printVV(v)                  for(auto &it: v) {printV(it);}
+#define all(v)                      v.begin(),v.end()
+#define inp(i)                      int i; cin >> i;
+#define inp2(i,j)                   int i,j; cin >> i >> j;
+#define inp3(i,j,k)                 int i,j,k; cin >> i >> j >> k;
+#define inp4(i,j,k,l)               int i,j,k,l; cin >> i >> j >> k >> l;
+#define inparr(arr, n)              inp(n); vi arr(n); for(auto &it: arr) cin >> it;
+#define MOD                         1000000007ll
+#define fill(a,b)       			memset(a, b, sizeof(a))
 using namespace std;
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
@@ -41,8 +45,6 @@ void __f (const char* names, Arg1&& arg1, Args&&... args)
 	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
 }
 
-
-
 void solve()
 {
 
@@ -53,6 +55,7 @@ int32_t main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	clock_t z = clock();
+	cout << fixed << setprecision(12);
 	//w(x)
 	{
 		solve();
